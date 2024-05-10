21b7cef12e30e384c8b7d31c9270e935fa0d5269
Your Name <you@example.com>
2024-05-10 19:48:22 +0200

    creation of product

--- src/main/java/com/didier/beststore/controllers/ProductController.java
+++ src/main/java/com/didier/beststore/controllers/ProductController.java
@@ -1,13 +1,24 @@
 package com.didier.beststore.controllers;
 
 import com.didier.beststore.models.Product;
+import com.didier.beststore.models.ProductDto;
 import com.didier.beststore.services.ProductRepository;
+import jakarta.validation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.nio.file.Path;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -19,8 +30,69 @@ public class ProductController {
 
     @GetMapping({"", "/"})
     public String showProductList(Model model){
-        List<Product> products = repo.findAll();
+        List<Product> products = repo.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("products", products);
         return "products/index";
+    }
+
+
+    @GetMapping("/create")
+    public String showCreatedPage(Model model){
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto", productDto);
+        return "products/CreateProduct";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(
+            @Valid @ModelAttribute ProductDto productDto,
+            BindingResult result
+    ){
+        if(productDto.getImageFile().isEmpty()){
+            result.addError(new FieldError("productDto", "imageFile","The image file is required"));
+        }
+
+        if(result.hasErrors()){
+            return "products/CreateProduct";
+        }
+
+        MultipartFile image = productDto.getImageFile();
+        Date createdAt = new Date();
+        String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+        try {
+            String uploadDir = "public/images/";
+            Path uploadPath = Paths.get(uploadDir);
+            if (!Files.exists (uploadPath)) {
+                Files.createDirectories (uploadPath);
+            }
+            try (InputStream inputStream = image.getInputStream()){
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception ex){
+            System.out.println("Exception:" + ex.getMessage());
+        }
+
+        Product product =new Product();
+        product.setName(productDto.getName());
+        product.setBrand(productDto.getBrand());
+        product.setCategory(productDto.getCategory());
+        product.setPrice(String.valueOf(productDto.getPrice()));
+        product.setDescription(productDto.getDescription());
+        product.setCreateAt(createdAt);
+        product.setImageFileName(storageFileName);
+
+        repo.save(product);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit")
+    public String showEditPage(
+            Model model,
+            @RequestParam int id
+    ){
+        return "products/EditProduct";
     }
 }
